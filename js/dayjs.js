1284799bc02d0d3da19bb787f9d7bac9fc4ddf4e
Hanson Li <lihsca@gmail.com>
2018-12-06 17:42:07 -0500

    first commit

---
+++ js/dayjs.js
@@ -0,0 +1,497 @@
+import * as C from './constant'
+/*
+export const SECONDS_A_MINUTE = 60
+export const SECONDS_A_HOUR = SECONDS_A_MINUTE * 60
+export const SECONDS_A_DAY = SECONDS_A_HOUR * 24
+export const SECONDS_A_WEEK = SECONDS_A_DAY * 7
+
+export const MILLISECONDS_A_SECOND = 1e3
+export const MILLISECONDS_A_MINUTE = SECONDS_A_MINUTE * MILLISECONDS_A_SECOND
+export const MILLISECONDS_A_HOUR = SECONDS_A_HOUR * MILLISECONDS_A_SECOND
+export const MILLISECONDS_A_DAY = SECONDS_A_DAY * MILLISECONDS_A_SECOND
+export const MILLISECONDS_A_WEEK = SECONDS_A_WEEK * MILLISECONDS_A_SECOND
+
+// English locales
+export const MS = 'millisecond'
+export const S = 'second'
+export const MIN = 'minute'
+export const H = 'hour'
+export const D = 'day'
+export const W = 'week'
+export const M = 'month'
+export const Q = 'quarter'
+export const Y = 'year'
+export const DATE = 'date'
+
+export const FORMAT_DEFAULT = 'YYYY-MM-DDTHH:mm:ssZ'
+
+export const INVALID_DATE_STRING = 'Invalid Date'
+
+// regex
+export const REGEX_PARSE = /^(\d{4})-?(\d{1,2})-?(\d{0,2})(.*?(\d{1,2}):(\d{1,2}):(\d{1,2}))?.?(\d{1,3})?$/
+export const REGEX_FORMAT = /\[.*?\]|Y{2,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
+
+export const en = {
+  name: 'en',
+  weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
+  months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_')
+}
+*/
+
+import U from './utils'
+/*
+import * as C from './constant'
+
+const padStart = (string, length, pad) => {
+  const s = String(string)
+  if (!s || s.length >= length) return string
+  return `${Array((length + 1) - s.length).join(pad)}${string}`
+}
+
+const padZoneStr = (negMinuts) => {
+  const minutes = Math.abs(negMinuts)
+  const hourOffset = Math.floor(minutes / 60)
+  const minuteOffset = minutes % 60
+  return `${negMinuts <= 0 ? '+' : '-'}${padStart(hourOffset, 2, '0')}:${padStart(minuteOffset, 2, '0')}`
+}
+
+const monthDiff = (a, b) => {
+  // function from moment.js in order to keep the same result
+  const wholeMonthDiff = ((b.year() - a.year()) * 12) + (b.month() - a.month())
+  const anchor = a.clone().add(wholeMonthDiff, 'months')
+  const c = b - anchor < 0
+  const anchor2 = a.clone().add(wholeMonthDiff + (c ? -1 : 1), 'months')
+  return Number(-(wholeMonthDiff + ((b - anchor) / (c ? (anchor - anchor2) :
+    (anchor2 - anchor)))) || 0)
+}
+
+const absFloor = n => (n < 0 ? Math.ceil(n) || 0 : Math.floor(n))
+
+const prettyUnit = (u) => {
+  const special = {
+    M: C.M,
+    y: C.Y,
+    w: C.W,
+    d: C.D,
+    h: C.H,
+    m: C.MIN,
+    s: C.S,
+    ms: C.MS
+  }
+  return special[u] || String(u || '').toLowerCase().replace(/s$/, '')
+}
+
+const isUndefined = s => s === undefined
+
+export default {
+  padStart,
+  padZoneStr,
+  monthDiff,
+  absFloor,
+  prettyUnit,
+  isUndefined
+}
+*/
+
+let L = 'en' // global locale
+const Ls = {} // global loaded locale
+Ls[L] = C.en
+
+const isDayjs = d => d instanceof Dayjs // eslint-disable-line no-use-before-define
+
+const parseLocale = (preset, object, isLocal) => {
+  let l
+  if (!preset) return null
+  if (typeof preset === 'string') {
+    if (Ls[preset]) {
+      l = preset
+    }
+    if (object) {
+      Ls[preset] = object
+      l = preset
+    }
+  } else {
+    const { name } = preset
+    Ls[name] = preset
+    l = name
+  }
+  if (!isLocal) L = l
+  return l
+}
+
+const dayjs = (date, c) => {
+  if (isDayjs(date)) {
+    return date.clone()
+  }
+  // eslint-disable-next-line no-nested-ternary
+  const cfg = c ? (typeof c === 'string' ? { format: c } : c) : {}
+  cfg.date = date
+  return new Dayjs(cfg) // eslint-disable-line no-use-before-define
+}
+
+const wrapper = (date, instance) => dayjs(date, { locale: instance.$L })
+
+const Utils = U // for plugin use
+Utils.parseLocale = parseLocale
+Utils.isDayjs = isDayjs
+Utils.wrapper = wrapper
+
+const parseDate = (date) => {
+  let reg
+  if (date === null) return new Date(NaN) // Treat null as an invalid date
+  if (Utils.isUndefined(date)) return new Date()
+  if (date instanceof Date) return date
+  // eslint-disable-next-line no-cond-assign
+  if ((typeof date === 'string')
+    && (/.*[^Z]$/i.test(date)) // looking for a better way
+    && (reg = date.match(C.REGEX_PARSE))) {
+    // 2018-08-08 or 20180808
+    return new Date(
+      reg[1], reg[2] - 1, reg[3] || 1,
+      reg[5] || 0, reg[6] || 0, reg[7] || 0, reg[8] || 0
+    )
+  }
+  return new Date(date) // timestamp
+}
+
+class Dayjs {
+  constructor(cfg) {
+    this.parse(cfg) // for plugin
+  }
+
+  parse(cfg) {
+    this.$d = parseDate(cfg.date)
+    this.init(cfg)
+  }
+
+  init(cfg) {
+    const { $d } = this
+    this.$y = $d.getFullYear()
+    this.$M = $d.getMonth()
+    this.$D = $d.getDate()
+    this.$W = $d.getDay()
+    this.$H = $d.getHours()
+    this.$m = $d.getMinutes()
+    this.$s = $d.getSeconds()
+    this.$ms = $d.getMilliseconds()
+    this.$L = this.$L || parseLocale(cfg.locale, null, true) || L
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  $utils() {
+    return Utils
+  }
+
+  isValid() {
+    return !(this.$d.toString() === C.INVALID_DATE_STRING)
+  }
+
+  isSame(that, units) {
+    const other = dayjs(that)
+    return this.startOf(units) <= other && other <= this.endOf(units)
+  }
+
+  isAfter(that, units) {
+    return dayjs(that) < this.startOf(units)
+  }
+
+  isBefore(that, units) {
+    return this.endOf(units) < dayjs(that)
+  }
+
+  year() {
+    return this.$y
+  }
+
+  month() {
+    return this.$M
+  }
+
+  day() {
+    return this.$W
+  }
+
+  date() {
+    return this.$D
+  }
+
+  hour() {
+    return this.$H
+  }
+
+  minute() {
+    return this.$m
+  }
+
+  second() {
+    return this.$s
+  }
+
+  millisecond() {
+    return this.$ms
+  }
+
+  unix() {
+    return Math.floor(this.valueOf() / 1000)
+  }
+
+  valueOf() {
+    // timezone(hour) * 60 * 60 * 1000 => ms
+    return this.$d.getTime()
+  }
+
+  startOf(units, startOf) { // startOf -> endOf
+    const isStartOf = !Utils.isUndefined(startOf) ? startOf : true
+    const unit = Utils.prettyUnit(units)
+    const instanceFactory = (d, m) => {
+      const ins = wrapper(new Date(this.$y, m, d), this)
+      return isStartOf ? ins : ins.endOf(C.D)
+    }
+    const instanceFactorySet = (method, slice) => {
+      const argumentStart = [0, 0, 0, 0]
+      const argumentEnd = [23, 59, 59, 999]
+      return wrapper(this.toDate()[method].apply( // eslint-disable-line prefer-spread
+        this.toDate(),
+        (isStartOf ? argumentStart : argumentEnd).slice(slice)
+      ), this)
+    }
+
+    switch (unit) {
+      case C.Y:
+        return isStartOf ? instanceFactory(1, 0) :
+          instanceFactory(31, 11)
+      case C.M:
+        return isStartOf ? instanceFactory(1, this.$M) :
+          instanceFactory(0, this.$M + 1)
+      case C.W:
+        return isStartOf ? instanceFactory(this.$D - this.$W, this.$M) :
+          instanceFactory(this.$D + (6 - this.$W), this.$M)
+      case C.D:
+      case C.DATE:
+        return instanceFactorySet('setHours', 0)
+      case C.H:
+        return instanceFactorySet('setMinutes', 1)
+      case C.MIN:
+        return instanceFactorySet('setSeconds', 2)
+      case C.S:
+        return instanceFactorySet('setMilliseconds', 3)
+      default:
+        return this.clone()
+    }
+  }
+
+  endOf(arg) {
+    return this.startOf(arg, false)
+  }
+
+  $set(units, int) { // private set
+    const unit = Utils.prettyUnit(units)
+    const name = {
+      [C.D]: 'setDate',
+      [C.DATE]: 'setDate',
+      [C.M]: 'setMonth',
+      [C.Y]: 'setFullYear',
+      [C.H]: 'setHours',
+      [C.MIN]: 'setMinutes',
+      [C.S]: 'setSeconds',
+      [C.MS]: 'setMilliseconds'
+    }[unit]
+    const arg = unit === C.D ? this.$D + (int - this.$W) : int
+
+    if (this.$d[name]) this.$d[name](arg)
+
+    this.init()
+    return this
+  }
+
+
+  set(string, int) {
+    return this.clone().$set(string, int)
+  }
+
+  add(number, units) {
+    number = Number(number) // eslint-disable-line no-param-reassign
+    const unit = Utils.prettyUnit(units)
+    const instanceFactory = (u, n) => {
+      const date = this.set(C.DATE, 1).set(u, n + number)
+      return date.set(C.DATE, Math.min(this.$D, date.daysInMonth()))
+    }
+    const instanceFactorySet = (n) => {
+      const date = new Date(this.$d)
+      date.setDate(date.getDate() + (n * number))
+      return wrapper(date, this)
+    }
+    if (unit === C.M) {
+      return instanceFactory(C.M, this.$M)
+    }
+    if (unit === C.Y) {
+      return instanceFactory(C.Y, this.$y)
+    }
+    if (unit === C.D) {
+      return instanceFactorySet(1)
+    }
+    if (unit === C.W) {
+      return instanceFactorySet(7)
+    }
+    const step = {
+      [C.MIN]: C.MILLISECONDS_A_MINUTE,
+      [C.H]: C.MILLISECONDS_A_HOUR,
+      [C.S]: C.MILLISECONDS_A_SECOND
+    }[unit] || 1 // ms
+
+    const nextTimeStamp = this.valueOf() + (number * step)
+    return wrapper(nextTimeStamp, this)
+  }
+
+  subtract(number, string) {
+    return this.add(number * -1, string)
+  }
+
+  format(formatStr) {
+    if (!this.isValid()) return C.INVALID_DATE_STRING
+
+    const str = formatStr || C.FORMAT_DEFAULT
+    const zoneStr = Utils.padZoneStr(this.$d.getTimezoneOffset())
+    const locale = this.$locale()
+    const {
+      weekdays, months
+    } = locale
+    const getShort = (arr, index, full, length) => (
+      (arr && arr[index]) || full[index].substr(0, length)
+    )
+    const get$H = (match) => {
+      if (this.$H === 0) return 12
+      return Utils.padStart(this.$H < 13 ? this.$H : this.$H - 12, match === 'hh' ? 2 : 1, '0')
+    }
+
+    return str.replace(C.REGEX_FORMAT, (match) => {
+      if (match.indexOf('[') > -1) return match.replace(/\[|\]/g, '')
+      return {
+        YY: String(this.$y).slice(-2),
+        YYYY: String(this.$y),
+        M: String(this.$M + 1),
+        MM: Utils.padStart(this.$M + 1, 2, '0'),
+        MMM: getShort(locale.monthsShort, this.$M, months, 3),
+        MMMM: months[this.$M],
+        D: String(this.$D),
+        DD: Utils.padStart(this.$D, 2, '0'),
+        d: String(this.$W),
+        dd: getShort(locale.weekdaysMin, this.$W, weekdays, 2),
+        ddd: getShort(locale.weekdaysShort, this.$W, weekdays, 3),
+        dddd: weekdays[this.$W],
+        H: String(this.$H),
+        HH: Utils.padStart(this.$H, 2, '0'),
+        h: get$H(match),
+        hh: get$H(match),
+        a: this.$H < 12 ? 'am' : 'pm',
+        A: this.$H < 12 ? 'AM' : 'PM',
+        m: String(this.$m),
+        mm: Utils.padStart(this.$m, 2, '0'),
+        s: String(this.$s),
+        ss: Utils.padStart(this.$s, 2, '0'),
+        SSS: Utils.padStart(this.$ms, 3, '0'),
+        Z: zoneStr
+      }[match] || zoneStr.replace(':', '') // 'ZZ'
+    })
+  }
+
+  diff(input, units, float) {
+    const unit = Utils.prettyUnit(units)
+    const that = dayjs(input)
+    const diff = this - that
+    let result = Utils.monthDiff(this, that)
+
+    result = {
+      [C.Y]: result / 12,
+      [C.M]: result,
+      [C.Q]: result / 3,
+      [C.W]: diff / C.MILLISECONDS_A_WEEK,
+      [C.D]: diff / C.MILLISECONDS_A_DAY,
+      [C.H]: diff / C.MILLISECONDS_A_HOUR,
+      [C.MIN]: diff / C.MILLISECONDS_A_MINUTE,
+      [C.S]: diff / C.MILLISECONDS_A_SECOND
+    }[unit] || diff // milliseconds
+
+    return float ? result : Utils.absFloor(result)
+  }
+
+  daysInMonth() {
+    return this.endOf(C.M).$D
+  }
+
+  $locale() { // get locale object
+    return Ls[this.$L]
+  }
+
+  locale(preset, object) {
+    const that = this.clone()
+    that.$L = parseLocale(preset, object, true)
+    return that
+  }
+
+  clone() {
+    return wrapper(this.toDate(), this)
+  }
+
+  toDate() {
+    return new Date(this.$d)
+  }
+
+  toArray() {
+    return [
+      this.$y,
+      this.$M,
+      this.$D,
+      this.$H,
+      this.$m,
+      this.$s,
+      this.$ms
+    ]
+  }
+
+  toJSON() {
+    return this.toISOString()
+  }
+
+  toISOString() {
+    // ie 8 return
+    // new Dayjs(this.valueOf() + this.$d.getTimezoneOffset() * 60000)
+    // .format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')
+    return this.$d.toISOString()
+  }
+
+  toObject() {
+    return {
+      years: this.$y,
+      months: this.$M,
+      date: this.$D,
+      hours: this.$H,
+      minutes: this.$m,
+      seconds: this.$s,
+      milliseconds: this.$ms
+    }
+  }
+
+  toString() {
+    return this.$d.toUTCString()
+  }
+}
+
+dayjs.prototype = Dayjs.prototype
+
+dayjs.extend = (plugin, option) => {
+  plugin(option, Dayjs, dayjs)
+  return dayjs
+}
+
+dayjs.locale = parseLocale
+
+dayjs.isDayjs = isDayjs
+
+dayjs.unix = timestamp => (
+  dayjs(timestamp * 1e3)
+)
+
+dayjs.en = Ls[L]
+
+export default dayjs
