20f41d65f2f1be8e7289e84b3b822e3d69a2c37d
Hanson Li <lihsca@gmail.com>
2018-12-06 17:42:07 -0500

    first commit

---
+++ js/spa/spa-vanilla-js-app.js
@@ -0,0 +1,61 @@
+/* App.js */
+import SearchView from "./ui/SearchView";
+import ResultsView from "./ui/ResultsView";
+import ItemCollection from "./state/ItemCollection";
+import "./App.scss";
+
+export default {
+
+    Create() {
+        return Object.assign(Object.create(null), this.methods);
+    },
+
+    methods: {
+
+        init() {
+            this.showSearchView();
+
+            window.onpopstate = () => {
+                this.showSearchView();
+            };
+        },
+
+        showSearchView(options = {noResults: false}){
+            this.route('search');
+
+            const searchView = SearchView.View();
+            searchView.render(options);
+            searchView.focusInput();
+
+            searchView.onSearch((query) => {
+                this.doSearch(query);
+            });
+        },
+
+        showResultsView(models){
+            this.route('results');
+            ResultsView.View().render(models);
+        },
+
+        doSearch(query = ''){
+            ItemCollection.Create().search(query).then((models) => {
+                if (models.length > 0) {
+                    this.showResultsView(models);
+                } else {
+                    this.showSearchView({noResults: true});
+                }
+            }).catch((err) => {
+                throw err;
+            });
+        },
+
+        route(route){
+            history.pushState(null, null, `/${route}/`);
+        }
+    }
+};
+
+/* index.js */
+import App from "./App";
+
+window.onload = () => { App.Create().init(); };
