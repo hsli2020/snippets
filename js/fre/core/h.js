4a231dae6ce31cee38e3fbdbac9446ae20db7b78
Hanson Li <lihsca@gmail.com>
2018-12-06 17:42:07 -0500

    first commit

---
+++ js/fre/core/h.js
@@ -0,0 +1,7 @@
+export function h(type, props, ...children) {
+  return {
+    type: type,
+    props,
+    children
+  }
+}
---
+++ js/fre/core/index.js
@@ -0,0 +1,5 @@
+import { h } from "./h"
+import { render } from "./render"
+import { useState } from "./hook"
+
+export { h, render, useState }
